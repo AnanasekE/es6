98503df713d5ada70345d0efb809ba544fe2d544
janek <janeksambor@gmail.com>
2022-04-04 11:46:17 +0200

    INIT

---
+++ script.js
@@ -0,0 +1,36 @@
+// const col1Seed = ['BMW', 'Mercedes', 'Porche', 'Audi']
+// const col2Seed = ['Ferrari', 'Lamboghini', 'Maserati', 'Abarth']
+//
+//
+//
+// const buildList = (car) => {return `<li>${car[i]}</li>`}
+// const col1 = ``
+//
+// const col2 = ``
+// const app = `
+// <div>
+//     <div>${col1}</div>
+//     <div>${col2}</div>
+// </div>
+// `
+//
+// document.getElementById('app').innerHTML = app
+
+
+// const person = {
+//     name: 'Adam',
+//     age: 24,
+//     iq: 180,
+//     mathDeegrees: [5, 4, 5, 6]
+// }
+//
+// const { name, age } = person
+// console.log(name, age)
+//
+// const cars = ['AMOGUS', 'SUS', 'SUSSY']
+// console.log(...cars)
+//
+// const fn = (arguments) => {
+//     console.log(...arguments)
+// }
+// fn(1,2,3,4,5,6,7,8,9)
